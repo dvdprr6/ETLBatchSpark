235ee5b9f5488a13ced2297a8570a406a7a6ea26
David Parr <dvdprr6@gmail.com>
2018-10-13 22:16:33 -0400

    add data stream implementation

--- src/main/scala/com/etl/batch/spark/dao/TripsDao.scala
+++ src/main/scala/com/etl/batch/spark/dao/TripsDao.scala
@@ -1,12 +1,15 @@
-package com.etl.batch.spark.dao.dataframe
+package com.etl.batch.spark.dao
 
 import com.etl.batch.spark.connection.SparkConnection
 import com.etl.batch.spark.util.Constants
-import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.{DataFrame, Row}
+import org.apache.spark.streaming.dstream.DStream
 
-class TripsDataFrameDao extends DataFrameDao {
-  override def get(): DataFrame = {
+class TripsDao extends Dao {
+  override def getDataFrame(): DataFrame = {
     return SparkConnection.getSparkSession.read.format(Constants.FORMAT_CSV).option("header", "true").load(Constants.TRIPS)
   }
+
+  override def getDStream(): DStream[Row] = throw new UnsupportedOperationException
 }
 
