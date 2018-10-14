ff712b4cd42b6bfcb9f020360297da29a539fae8
David Parr <dvdprr6@gmail.com>
2018-10-13 22:16:11 -0400

    everything from the session

--- src/main/scala/com/etl/batch/spark/connection/SparkConnection.scala
+++ src/main/scala/com/etl/batch/spark/connection/SparkConnection.scala
@@ -5,7 +5,7 @@ import org.apache.spark.SparkContext
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 
-object SparkSessionConnection{
+object SparkConnection{
   private lazy val session: SparkSession = SparkSession.builder().appName(Constants.APP_NAME).enableHiveSupport().getOrCreate()
   private lazy val context: SparkContext = session.sparkContext
   private lazy val streaming: StreamingContext = new StreamingContext(context, Seconds(Constants.STREAMING_INTERVAL))
