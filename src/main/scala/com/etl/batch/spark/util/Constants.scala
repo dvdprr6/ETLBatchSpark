499af77661b365e4cc15540b595a7291785bd12b
David Parr <dvdprr6@gmail.com>
2018-10-13 22:15:56 -0400

    additional constants

--- src/main/scala/com/etl/batch/spark/util/Constants.scala
+++ src/main/scala/com/etl/batch/spark/util/Constants.scala
@@ -7,7 +7,9 @@ object Constants {
   val TRIPS: String = "hdfs://localhost:9000/user/vagrant/stm/gtfs/staging/trips/trips.txt"
   val CALENDAR_DATES: String = "hdfs://localhost:9000/user/vagrant/stm/gtfs/staging/calendar_dates/calendar_dates.txt"
   val FREQUENCIES: String = "hdfs://localhost:9000/user/vagrant/stm/gtfs/staging/frequencies/frequencies.txt"
-  val ENRICHED_TRIPS: String = "hdfs://localhost:9000/user/vagrant/stm/gtfs/staging/enriched_trips"
+  val ENRICHED_TRIPS: String = "hdfs://localhost:9000/user/vagrant/stm/gtfs/staging/enriched_trips/"
+  val ENRICHED_STOP_TIMES: String = "hdfs://localhost:9000/user/vagrant/stm/gtfs/staging/enriched_stop_times/"
+  val STOP_TIMES: String = "hdfs://localhost:9000/user/vagrant/stm/gtfs/staging/stop_times/"
 
   val TRIPS_ROUTE_ID: String = "route_id"
   val TRIPS_SERVICE_ID: String = "service_id"
@@ -23,6 +25,13 @@ object Constants {
   val FREQUENCIES_START_TIME: String = "start_time"
   val FREQUENCIES_END_TIME: String = "end_time"
   val FREQUENCIES_HEADWAY_SECS: String = "headway_secs"
+  val STOP_TIMES_TRIP_ID: String = "trip_id"
+  val STOP_TIMES_ARRIVAL_TIME: String = "arrival_time"
+  val STOP_TIMES_DEPARTURE_TIME: String = "departure_time"
+  val STOP_TIMES_STOP_ID: String = "stop_id"
+  val STOP_TIMES_STOP_SEQUENCIES: String = "stop_sequence"
 
   val STREAMING_INTERVAL: Int = 10
+
+  val STOP_TIMES_SCHEMA: String = "trip_id,arrival_time,departure_time,stop_id,stop_sequence"
 }
