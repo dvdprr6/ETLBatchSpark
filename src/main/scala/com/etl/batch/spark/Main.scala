db652c1c5d0b8dde7077e49588148b5cc96139a1
David Parr <dvdprr6@gmail.com>
2018-10-14 11:15:46 -0400

    print each stoptime record to hfds

--- src/main/scala/com/etl/batch/spark/Main.scala
+++ src/main/scala/com/etl/batch/spark/Main.scala
@@ -17,22 +17,6 @@ object Main extends App{
 
   var enrichedTripsDF = EnrichTripsBuilderFactory.getTripsBuilder()
     .setTrips(tripsDF).setCalendarDates(calendarDatesDF).setFrequencies(frequenciesDF).build.enrich()
-    .toDF(
-      Constants.TRIPS_ROUTE_ID,
-      Constants.TRIPS_SERVICE_ID,
-      Constants.TRIPS_TRIP_ID,
-      Constants.TRIPS_TRIP_HEADSIGN,
-      Constants.TRIPS_DIRECTION_ID,
-      Constants.TRIPS_SHAPE_ID,
-      Constants.TRIPS_WHEELCHAIR_ACCESSIBLE,
-      Constants.TRIPS_NOTE_FR,
-      Constants.TRIPS_NOTE_EN,
-      Constants.CALENDAR_DATES_DATE,
-      Constants.CALENDAR_DATES_EXCEPTION_TYPE,
-      Constants.FREQUENCIES_START_TIME,
-      Constants.FREQUENCIES_END_TIME,
-      Constants.FREQUENCIES_HEADWAY_SECS
-    )
 
   enrichedTripsDF.rdd.map(x => x.mkString(",")).saveAsTextFile(Constants.ENRICHED_TRIPS)
 
@@ -41,13 +25,8 @@ object Main extends App{
 
   stopTimesDS.foreachRDD{ rdd =>
     var stopTimesDF = SparkConnection.getSparkSession.createDataFrame(rdd, schema)
-      .toDF(
-        Constants.STOP_TIMES_TRIP_ID,
-        Constants.STOP_TIMES_ARRIVAL_TIME,
-        Constants.STOP_TIMES_DEPARTURE_TIME,
-        Constants.STOP_TIMES_STOP_ID,
-        Constants.STOP_TIMES_STOP_SEQUENCIES
-      )
+
+    stopTimesDF.rdd.map(x => x.mkString(","))saveAsTextFile(Constants.STOP_TIMES_TEST)
 
     var enrichedStoptimes = EnrichStoptimesBuilderFactory.getStoptimesBuilder()
       .setEnrichedTrips(enrichedTripsDF).setStoptimes(stopTimesDF).build.enrich()
